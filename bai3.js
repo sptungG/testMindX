d0e661468cd938c4cc6e06387468dbfe4031fb0f
sptungG <ngoviettung154@gmail.com>
2021-02-25 23:18:57 +0700

    update bai 3

--- bai3.js
+++ bai3.js
@@ -6,38 +6,40 @@ function createRandomNumber(){
 }
 
 function checkNumber(){
-    clicked++
-    document.querySelector('.btn').innerHTML = 'Check(' + clicked + ')'
-    var inputValue = Number(document.getElementById('inputValue').value);
+    clicked++;
+    document.getElementById('btn').innerHTML = 'Check(' + clicked + ')';
+    
+    let inputValue = Number(document.getElementById('inputValue').value);
+    
     document.getElementById('inputValue').focus();
     document.getElementById('inputValue').placeholder = "You previously entered "+inputValue;
     document.getElementById('inputValue').value = '';
     console.log(inputValue)
     if(typeof(inputValue) != String){
         if((inputValue > -1) && (inputValue < 11) ){
-            document.querySelector('.btn').style.color = "white"
+            document.querySelector('.btn').style.color = "white";
             if(randomNumber == inputValue){
-                document.getElementById('status').innerHTML = 'Hurrahh!! You found me.'
-                document.querySelector('.btn').style.backgroundColor = "green"
+                document.getElementById('status').innerHTML = 'Hurrahh!! You found me.';
+                document.getElementById('btn').style.backgroundColor = "green";
                 setTimeout(function(){
                     location.reload()
                 },3000)
             } else if(Math.abs(randomNumber - inputValue) >= 5) {
-                document.getElementById('status').innerHTML = 'You are far away.'
-                document.querySelector('.btn').style.backgroundColor = "tomato"
+                document.getElementById('status').innerHTML = 'You are far away.';
+                document.getElementById('btn').style.backgroundColor = "tomato";
             }else if(Math.abs(randomNumber - inputValue) >= 3) {
-                document.getElementById('status').innerHTML = 'You are close.'
-                document.querySelector('.btn').style.backgroundColor = "orange"
+                document.getElementById('status').innerHTML = 'You are close.';
+                document.getElementById('btn').style.backgroundColor = "orange";
             }else if(Math.abs(randomNumber - inputValue) >= 1) {
-                document.getElementById('status').innerHTML = 'You are too close, come on crack this.'
-                document.querySelector('.btn').style.backgroundColor = "rgb(126, 160, 57)"
+                document.getElementById('status').innerHTML = 'You are too close, come on crack this.';
+                document.getElementById('btn').style.backgroundColor = "rgb(126, 160, 57)";
             }
         }
         else{
-            document.getElementById('status').innerHTML = 'Please type in between (1-10)'
+            document.getElementById('status').innerHTML = 'Please type in between (1-10)';
         } 
     }else {
-        document.getElementById('status').innerHTML = 'Please Enter a Number.'
+        document.getElementById('status').innerHTML = 'Please Enter a Number.';
     }
    
     
@@ -45,7 +47,8 @@ function checkNumber(){
 
 function key(e){
     if(e.keyCode === 13){
-        checkNumber()
+        // Enter key on keyboard.
+        checkNumber();
     }
 }
-document.addEventListener('keyup', key)
+document.addEventListener('keyup', key);
